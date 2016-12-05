15e0b0344850e9be2973f26d9e63f6755f26a193
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2016-12-05 22:31:25 +0545

    Configured repo with doxygen.  Changes to be committed: 	new file:   LICENSE.MD 	new file:   Makefile 	modified:   README.MD 	modified:   doxy.config 	new file:   src/daemon/daemon.cc

---
+++ Makefile
@@ -0,0 +1,27 @@
+CC = g++ -Wall 
+SRC=src
+BUILD_DIR=build
+BUILD_ENV=debug
+
+all: daemon camera manager docs
+
+prepare:
+	sudo apt install graphviz libopencv-dev qtcreator libboost-dev
+
+daemon:
+	$(CC) -c $(SRC)/daemon/daemon.cc -o $(BUILD_DIR)/$(BUILD_ENV)/daemon.o
+
+camera:
+
+manager:
+
+install: daemon camera manager
+
+clean: clean-docs
+	rm -rf *.o
+
+clean-docs:
+	rm -rf docs/
+
+docs:
+	doxygen doxy.config
---
+++ src/daemon/daemon.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+#include <sys/types.h>
+#include <unistd.h>
+
+
+using namespace std;
+/**
+ * Daemon process that executes in the background.
+ * Returns immediately forking a child daemon process. The parent process ceases to exist
+ * while the child process runs as daemon process for serving its purpose.
+ * The functions of this daemon is to 
+ *		- watch for given folder
+ *		- respond to changes by updating database
+ *
+ */
+int main() {
+   pid_t pID = fork();
+   if (pID == 0) {
+    } else if (pID < 0) {
+        cerr << "Failed to fork" << endl; // Error while initializing, exit the parent.
+        exit(1);
+    } else {
+      exit(0);
+    }
+	sleep(5);
+	cout << "elasdf" << endl;
+    return 0;
+}
