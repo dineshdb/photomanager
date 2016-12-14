78492861846bb7faa894ac34683999ddb2f0de58
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2016-12-14 16:12:01 +0545

    Added manager folder

---
+++ src/manager/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+/**
+ * The entry point for our camera application.
+ * It opens the camera stream for picture caption.
+ * It can detect faces in the camera and identify the person.
+ * Image will be captured and saved on user-action.
+ */
+int main(){
+	std::cout << "Hello";
+	return 0;
+}
