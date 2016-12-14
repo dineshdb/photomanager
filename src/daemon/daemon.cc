246d348765fed3dfe68f419e4e9d16a0587896b4
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2016-12-14 16:10:42 +0545

    Added a function to find number of facess in an image

--- src/daemon/daemon.cc
+++ src/daemon/daemon.cc
@@ -1,31 +1,37 @@
 #include <iostream>
 #include <string>
 
-#include <sys/types.h>
-#include <unistd.h>
+#include <boost/filesystem.hpp>
+#include <boost/range/iterator_range.hpp>
 
-
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 
 using namespace std;
+using namespace cv;
+
+namespace fs = boost::filesystem;
 /**
  * Daemon process that executes in the background.
- * Returns immediately forking a child daemon process. The parent process ceases to exist
- * while the child process runs as daemon process for serving its purpose.
  * The functions of this daemon is to 
  *		- watch for given folder
  *		- respond to changes by updating database
+ *	Uses libuv for async I?O and inotify/epoll for dir watching
  *
  */
-int main() {
-   pid_t pID = fork();
-   if (pID == 0) {
-    } else if (pID < 0) {
-        cerr << "Failed to fork" << endl; // Error while initializing, exit the parent.
-        exit(1);
-    } else {
-      exit(0);
-    }
-	sleep(5);
-	cout << "elasdf" << endl;
+int main(int argc, char *argv[]) {
+
     return 0;
 }
+
+std::vector<Rect> getFaces(const string &str){
+	std::vector<Rect> faces;
+	Mat frame = cv::imread(str);
+	cvtColor(frame, frame, cv::COLOR_BGR2GRAY);
+	equalizeHist( frame, frame );
+    face_cascade.detectMultiScale( frame, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+	return faces;
+}
+
