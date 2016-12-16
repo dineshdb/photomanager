6688a674b821e6a11f3b9e87abb296928b91e2d3
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2016-12-16 16:26:40 +0545

    Added method to crop only the face from given frame. 	modified:   .gitignore 	modified:   src/daemon/daemon.cc

--- src/daemon/daemon.cc
+++ src/daemon/daemon.cc
@@ -26,6 +26,13 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+/**
+ * @brief Detects the faces available in an image.
+ * This does not try to recognize the faces, it only detects them.
+ * @param Matrix of image to identify faces.
+ * @return an vector of faces contained in the given image.
+ */
+
 std::vector<Rect> getFaces(const string &str){
 	std::vector<Rect> faces;
 	Mat frame = cv::imread(str);
@@ -33,5 +40,20 @@ std::vector<Rect> getFaces(const string &str){
 	equalizeHist( frame, frame );
     face_cascade.detectMultiScale( frame, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
 	return faces;
+}
+
+
+/**
+ * @brief Crops the face for recognition.
+ * @param image Image to be cropped from
+ * @param faces to be cropped
+ * @return vector of cropped faces.
+ */
+Mat cropFace(Mat image, Rect face){
+	Point tl(face.x, face.y);
+	Point dr(face.x + face.width, face.y + face.height);
+	Rect f(tl, dr);
+	Mat croppedFace = image(f);
+	return croppedFace;
 }
 
