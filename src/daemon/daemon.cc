8a7658f64c446529a0f20d48df5ffbb425ce032b
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2016-12-23 22:23:34 +0545

    Updated docs for daemon

--- src/daemon/daemon.cc
+++ src/daemon/daemon.cc
@@ -14,7 +14,16 @@ using namespace std;
  * The functions of this daemon is to
  *		- watch for given folder
  *		- respond to changes by updating database
+ * Use this daemon as a dbus, server or a normal command line 
+ * program.
+ * To run this program as a dbus daemon, run `$ daemon -d `
+ * You can check it's interface using d-feet program. Open dfeet and 
+ * browse to com.dinnux.PhotoManager and see it's exposed functionality.
  *
+ * To update the database with face, run `$ daemon -u <image> -i <userid>`
+ * The userid field is compulsory but not functional yet.
+ *
+ * To predict a photo, run `$ daemon -p <image>`
  *
  */
 int main(int argc, char *argv[]) {
