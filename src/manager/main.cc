5e24a9ecf0ed53617cfbc75e96a472f8b1ab6a54
atosh502 <aashutoshpoudyal@gmail.com>
2016-12-30 19:19:04 +0545

    added manager ui files   Changes to be committed: 	modified:   src/manager/CMakeLists.txt 	modified:   src/manager/main.cc 	new file:   src/manager/manager.glade

--- src/manager/main.cc
+++ src/manager/main.cc
@@ -1,12 +1,54 @@
-#include <iostream>
-#include <iomanip>
+#include <gtkmm.h>
 /**
- * The entry point for our camera application.
- * It opens the camera stream for picture caption.
- * It can detect faces in the camera and identify the person.
- * Image will be captured and saved on user-action.
+ *	This is the main GUI for our photomanager application.
+ *	It has buttons: titled "People", "Tags", "Open Camera", "Folders"
+ *	Button "People" displays all pictures with specific faces or names that are identified and stored in the database
+ *	Button "Tags" displays all pictures related with any tags e.g. an event or ocassion
+ * 	Button "Open Camera" opens the device camera to capture a photo followed by options to store at specific location or edit
+ *	Button "Folders" opens the view of folders that are under constant watch of the underlying daemon of our PhotoManager app
  */
-int main(){
-	std::cout << "Hello";
-	return 0;
+
+Gtk::Window* pWindow = nullptr;
+
+int main (int argc, char **argv)
+{
+  auto app = Gtk::Application::create(argc, argv, "com.skynet.manager");
+
+  //Load the Gtk::Builder file and instantiate its widgets
+  auto refBuilder = Gtk::Builder::create();
+  
+  try
+  {
+    refBuilder->add_from_file("manager.glade");
+  }
+  catch(const Glib::FileError& ex)
+  {
+    std::cerr << "FileError: " << ex.what() << std::endl;
+    return 1;
+  }
+  catch(const Glib::MarkupError& ex)
+  {
+    std::cerr << "MarkupError: " << ex.what() << std::endl;
+    return 1;
+  }
+  catch(const Gtk::BuilderError& ex)
+  {
+    std::cerr << "BuilderError: " << ex.what() << std::endl;
+    return 1;
+  }
+
+  //Get the GtkBuilder-instantiated window
+  
+  refBuilder->get_widget("mainWindow", pWindow);
+  
+  if(pWindow)
+  {
+    app->run(*pWindow);
+  }
+
+  delete pWindow;
+
+  return 0;
 }
+
+
