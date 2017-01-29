1aed9e25f2ffdbdf70698c99bd1405e7dc7a1ec2
Dinesh Bhattarai <dineshbhattrai0@gmail.com>
2017-01-29 21:58:20 +0545

    Added a directory scanner class

---
+++ src/daemon/DirectoryScanner.hh
@@ -0,0 +1,41 @@
+#ifndef DIRECTORY_SCANNER
+#define DIRECTORY_SCANNER
+
+#include <stack>
+#include <vector>
+#include <glibmm.h>
+
+class DirectoryScanner {
+  std::stack<Glib::ustring> directories;
+  std::vector<Glib::ustring> files;
+
+public:
+  DirectoryScanner(){}
+  
+  void addFolder(Glib::ustring dir){
+    directories.push(dir);
+  }
+  
+  void start(){
+    while(directories.size() != 0){
+      Glib::ustring dirname = directories.top();
+      directories.pop();
+      Glib::Dir dir(dirname);
+      
+      Glib::ustring entry;
+      while(( entry = dir.read_name()) != ""){
+        Glib::ustring fullURL = dirname + "/" + entry;
+        if (Glib::file_test(fullURL, Glib::FILE_TEST_IS_DIR)){
+          addFolder(fullURL);
+        } else {
+          files.push_back(fullURL);
+        }
+      }
+    }
+  }
+  
+  auto getFiles(){
+     return files;
+  }
+};
+#endif
